09b6c878defe367786a7f28eec564512dc6b49a6
Jacob Dorris <jcbdrrs@gmail.com>
2024-06-06 10:16:03 -0500

    asdfdasdf

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -4,18 +4,10 @@ import NavBar from "../components/Nav/NavBar.jsx";
 import TasksContainer from "../components/tasks/TasksContainer.jsx";
 import { EventList } from "../events/EventList.jsx";
 import { NewEvent } from "../events/NewEvent.jsx";
-import { useState, useEffect } from "react";
+
 
 
 export default function ApplicationViews() {
-
-  const [currentUser, setCurrentUser] = useState({});
-
-  useEffect(() => {
-    const localNutshellUser = localStorage.getItem("loggedInUser");
-    const learningUserObject = JSON.parse(localNutshellUser);
-    setCurrentUser(learningUserObject);
-  }, []);
 
   const [currentUser, setCurrentUser] = useState({});
 
