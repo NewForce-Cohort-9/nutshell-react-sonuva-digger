0f042833bb224f502734944cd49d28921d0e1f00
LJ White <theljwhite@gmail.com>
2024-06-06 10:42:16 -0400

    style changes to account for task lists blue card refactor

--- src/components/tasks/TasksContainer.jsx
+++ src/components/tasks/TasksContainer.jsx
@@ -11,8 +11,27 @@ export default function TasksContainer({ currentUser }) {
     <div className="tasks-container">
       <section className="tasks-section">
         <div className="tasks-section-inner">
-          <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <h1 style={{ padding: "1rem 0 2rem 0", color: "#FFF" }}>
+              Uncompleted Tasks
+            </h1>
             <TasksList currentUser={currentUser} />
+          </div>
+        </div>
+      </section>
+      <section className="tasks-section">
+        <div className="tasks-section-inner">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h1 style={{ padding: "1rem 0 2rem 0", color: "#FFF" }}>
+              Completed Tasks
+            </h1>
+            {/* <TasksList currentUser={currentUser} /> */}
+            {/* Other TasksList here */}
           </div>
         </div>
       </section>
