593b2e8215b9a180195d9de5d937a821b417b65e
LJ White <theljwhite@gmail.com>
2024-06-07 15:31:26 -0400

    cleanup

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,13 +1,19 @@
+import "./splash.css";
+
 export const Splash = () => {
-    return (
+  return (
     <>
-    <div className="wrapper-center">
+      <div className="wrapper-center">
         <div className="splash">
-            <h1><span>Welcome to </span>
-            <span>Nutshell</span></h1>
-            <div>Your #1 Social app for news, events and tasks management</div>
+          <h1>
+            <span>Welcome to </span>
+            <span>Nutshell</span>
+          </h1>
+          <div style={{ color: "cornsilk" }}>
+            Your #1 Social app for news, events and tasks management
+          </div>
         </div>
-    </div>
+      </div>
     </>
-    )
-}
+  );
+};
